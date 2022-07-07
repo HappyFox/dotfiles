f95091d43d0bcd356cdda3485122553a84cf77c3
Keith Baston <baston@CHONK.localdomain>
2022-07-07 14:30:57 -0700

    adding fish.config for powerline

---
+++ config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_function_path $fish_function_path "/home/baston/.local/lib/python3.10/site-packages/powerline/bindings/fish"
+    powerline-setup
+end
